60fff0b2f6e7e5dc10b143d4453262b3ed3c4aad
Sephin Sebastian E <sephinsebastian0830@gmail.com>
2024-06-15 05:20:36 +0530

    adding render.com support 3

---
+++ project-s4-frontend/src/Components/Rules.js
@@ -0,0 +1,38 @@
+import React from "react";
+import './Rules.css';
+const Rule=()=>{
+
+
+
+return(
+    <div className="ruleContainer">
+        <div className="ruleBox">
+            <h1 className="rulesHeading">INSTRUCTION FOR MESS INMATES</h1>
+            <div className="rulesContent">
+                <ol>
+                    <li>CAUTION DEPOSIT WILL BE RETURNED ONLY BY THE END OF THE ACADEMIC YEAR.</li>
+                    <li>ANY CORRECTION IN THE MESS BILL SHOULD BE INFORMED WITHIN 5 DAYS AFTER PUBLISHING THE MESS BILL.</li>
+                    <li>MESS INMATES ARE REQUESTED TO CONTACT THE MESS SECRETARY FOR BILL PAYMEMNT IF THEY ARE OUT OF STATION.</li>
+                    <li>FINE IMPOSED WILL BE FINAL.</li>
+                    <li>SHARING MEALS ARE NOT PERMITTED.</li>
+                    <li>INMATES ARE NOT ALLOWED TO TAKE MESS UTENSILS OUT OF MESS. </li>
+                    <li>HAVING FOOD AFTER MESS TIME IS STRICTLY PROHIBITED FOR ALL INMATES. STRICT ACTIONS WILL BE TAKEN.</li>
+                    <li>MESS INMATES ARE REQUESTED TO UPDATE THE MESS REGISTER WITH JOINING DATE, EXTRAS, SELF GUESTS, GUESTS AND REDUCTIONS. FAILURE TO DO SO WILL BE FINED.</li>
+                    <li>MESS INMATES ARE REQUESTED TO HAVE FOOD INSIDE THE MESS. NOBODY IS PERMITTED TO TAKE FOOD OUT OF THE MESS.</li>
+                    <li>ANY ADJUSTMENT IN MESS TIMING FOR ANY DEPARTMENT STUDENTS MUST BE ADDRESSED TO THE MESS COMMITTEE IN ADVANCE.</li>
+                    <li>ALL MESS INMATES MUST SHOW MESS ID CARD BEFORE HAVING FOOD.</li>
+                    <li>EXTRAS PROVIDED WILL BE LIMITED, SO IT WILL BE BASED ON FIRST COME FIRST SERVE.</li>
+                    <li>FEASTS WILL BE PROVIDED ONCE IN A MONTH AND THE DATE WILL BE INTIMATED BY THE MESS SECRETARY. </li>
+                    <li>LATE MESS SHOULD BE INFORMED TO THE MESS COMMITTEE IN ADVANCE.</li>
+                    <li>MESS CUT WILL ONLY BE APPLICABLE FOR A MINIMUM OF 3 CONSECUTIVE DAYS.</li>
+                    <li>MESS CUT SHALL BE ENTERED IN THE MESS REGISTER AND SHOULD BE INFORMED TO THE MESS COMMITTEE IN ADVANCE.</li>
+                    <li>DECISION OF THE MESS SECRETARY WILL BE FINAL.</li>
+                </ol>
+            </div>
+        </div>
+    </div>
+
+);
+};
+
+export default Rule;
